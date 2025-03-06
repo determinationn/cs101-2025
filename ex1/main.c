68440abcb97983f71ae52ec3cabaff6e64569233
determinationn <austin301733@gmail.com>
2025-03-06 15:38:10 +0800

    Add files via upload

--- ex1/main.c
+++ ex1/main.c
@@ -3,33 +3,46 @@
 int main() {
     FILE *fp;
 
-    int a[] = {0, 1, 2};
-    char b[] = "ABC";
-    float c[] = {1.1, 1.2, 1.3};
+    int arr_write_int[3] = {0, 1, 2};
+    char arr_write_char[3] = {'A', 'B', 'C'};
+    float arr_write_float[3] = {1.1, 1.2, 1.3};
 
-    fp = fopen("a.bin", "wb");
+    int arr_read_int[3];
+    char arr_read_char[3];
+    float arr_read_float[3];
 
-    fwrite(a, sizeof(int), 3, fp);
-    fwrite(b, sizeof(char), 3, fp);
-    fwrite(c, sizeof(float), 3, fp);
+    fp = fopen("a.bin", "wb+");
+    if (fp == NULL) {
+        printf("無法開啟檔案！\n");
+        return 1;
+    }
+
+    fwrite(arr_write_int, sizeof(int), 3, fp);
+    fwrite(arr_write_char, sizeof(char), 3, fp);
+    fwrite(arr_write_float, sizeof(float), 3, fp);
+
+    fseek(fp, 0, SEEK_SET);
+
+    fread(arr_read_int, sizeof(int), 3, fp);
+    fread(arr_read_char, sizeof(char), 3, fp);
+    fread(arr_read_float, sizeof(float), 3, fp);
 
     fclose(fp);
 
-    int ra[3];
-    char rb[3];
-    float rc[3];
+    for (int i = 0; i < 3; i++) {
+        printf("%d ", arr_read_int[i]);
+    }
+    printf("\n");
 
-    fp = fopen("a.bin", "rb");
+    for (int i = 0; i < 3; i++) {
+        printf("%c ", arr_read_char[i]);
+    }
+    printf("\n");
 
-    fread(ra, sizeof(int), 3, fp);
-    fread(rb, sizeof(char), 3, fp);
-    fread(rc, sizeof(float), 3, fp);
-
-    fclose(fp);
-
-    printf("%d %d %d\n", ra[0], ra[1], ra[2]);  
-    printf("%c %c %c\n", rb[0], rb[1], rb[2]);  
-    printf("%.6f %.6f %.6f\n", rc[0], rc[1], rc[2]); 
+    for (int i = 0; i < 3; i++) {
+        printf("%.6f ", arr_read_float[i]); 
+    }
+    printf("\n");
 
     return 0;
 }
